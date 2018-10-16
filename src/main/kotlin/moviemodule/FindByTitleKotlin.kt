d7fb23c902964a384f02dc084fc6b6d5c585f450
jwin <jwin1968@gmail.com>
2018-10-16 23:43:45 +0200

    extract method

--- src/main/kotlin/moviemodule/FindByTitleKotlin.kt
+++ src/main/kotlin/moviemodule/FindByTitleKotlin.kt
@@ -1,15 +1,21 @@
 package moviemodule
 
 
-fun findByTitle(query: String, collection: MutableList<Movie>): List<Movie> {
+fun findByTitle(query: String, collection: MutableList<Movie>): List<Movie>{
     var results: MutableList<Movie> = mutableListOf()
 
     do {
         val movie = collection.removeAt(0)
-        if (movie.title.contains(query)) {
-            results.add(movie)
-        }
-    } while (collection.size > 0)
+        addIfMatches(query, movie, results)
+    }
+    while (collection.size > 0)
 
     return results
 }
+
+fun addIfMatches(query: String, movie: Movie, results: MutableList<Movie>){
+    if (movie.title.contains(query)){
+        results.add(movie)
+    }
+}
+
