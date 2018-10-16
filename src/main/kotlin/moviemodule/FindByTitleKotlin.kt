47cd902fbbf6d46e3077c1c200e4261ab71edec8
jwin <jwin1968@gmail.com>
2018-10-17 00:04:38 +0200

    remove duplicate 'movie' passing.. thanks to currying

--- src/main/kotlin/moviemodule/FindByTitleKotlin.kt
+++ src/main/kotlin/moviemodule/FindByTitleKotlin.kt
@@ -12,20 +12,20 @@ fun findByTitle(query: String, collection: MutableList<Movie>): List<Movie> {
 
     for (movie: Movie in collection) {
         val fn = addIf(predicate, query, movie, add)
-        results = fn(movie)(results)
+        results = fn(results)
     }
 
     return results
 }
 
-fun addIf(predicate: (String, Movie) -> Boolean, query: String, movie: Movie, add: (Movie) -> (List<Movie>) -> List<Movie>): (Movie) -> (List<Movie>) -> List<Movie> {
+fun addIf(predicate: (String, Movie) -> Boolean, query: String, movie: Movie, add: (Movie) -> (List<Movie>) -> List<Movie>): (List<Movie>) -> List<Movie> {
     if (predicate(query, movie)) {
-        return add
+        return add(movie)
     }
     return nop()
 }
 
-fun nop() = fun(_: Movie) = fun(_: List<Movie>) = listOf<Movie>()
+fun nop() = fun(_: List<Movie>) = listOf<Movie>()
 
 fun matches(query: String, movie: Movie) = movie.title.contains(query)
 
