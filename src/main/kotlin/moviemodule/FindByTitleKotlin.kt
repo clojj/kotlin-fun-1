293ed7cdb76715470a8438cdc8e38700ebac79a2
jwin <jwin1968@gmail.com>
2018-10-16 23:52:18 +0200

    moving up side-effect functions as first class citizens

--- src/main/kotlin/moviemodule/FindByTitleKotlin.kt
+++ src/main/kotlin/moviemodule/FindByTitleKotlin.kt
@@ -1,21 +1,34 @@
 package moviemodule
 
 
+// findByTitle :: (String, [Movie]) -> [Movie]
 fun findByTitle(query: String, collection: MutableList<Movie>): List<Movie>{
     var results: MutableList<Movie> = mutableListOf()
 
-    do {
-        val movie = collection.removeAt(0)
-        addIfMatches(query, movie, results)
+    // Use of functions as variables (predicate and add) : functions as first class citizen
+
+    // matches :: (String, Film) -> Boolean
+    val predicate = ::matches
+
+    // TODO side effect moved up (still on results)
+    // add :: (Film) -> Boolean
+    val add = fun (movie: Movie) = results.add(movie)
+
+    for (movie: Movie in collection){
+        val fn = addIf(predicate, query, movie, add)
+        fn(movie)
     }
-    while (collection.size > 0)
 
     return results
 }
 
-fun addIfMatches(query: String, movie: Movie, results: MutableList<Movie>){
-    if (movie.title.contains(query)){
-        results.add(movie)
+// addIf :: ((String, Movie) -> Boolean, String, Movie, [Movie] -> (Boolean)) -> (Movie) -> (Boolean)
+fun addIf(predicate: (String, Movie) -> Boolean, query: String, movie: Movie, add: (Movie) -> (Boolean)): (Movie) -> (Boolean){
+    if (predicate(query, movie)){
+        return add
     }
+    return fun (_: Movie) = false
 }
+
+fun matches(query: String, movie: Movie) = movie.title.contains(query)
 
