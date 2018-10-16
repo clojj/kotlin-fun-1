bb30a81fd5acce4fee9964c9bd24a6a20646d82d
jwin <jwin1968@gmail.com>
2018-10-17 00:01:16 +0200

    currying functions 'add' and 'nop'

--- src/main/kotlin/moviemodule/FindByTitleKotlin.kt
+++ src/main/kotlin/moviemodule/FindByTitleKotlin.kt
@@ -2,32 +2,30 @@ package moviemodule
 
 
 // findByTitle :: (String, [Movie]) -> [Movie]
-fun findByTitle(query: String, collection: MutableList<Movie>): List<Movie>{
+fun findByTitle(query: String, collection: MutableList<Movie>): List<Movie> {
     var results: List<Movie> = listOf()
-
-    // Use of functions as variables (predicate and add) : functions as first class citizen
 
     // matches :: (String, Film) -> Boolean
     val predicate = ::matches
 
-    // add :: (Film) -> Boolean
-    val add = fun (movie: Movie, movies: List<Movie>) = movies.plus(movie)
+    val add = fun(movie: Movie) = fun(movies: List<Movie>) = movies.plus(movie)
 
-    for (movie: Movie in collection){
+    for (movie: Movie in collection) {
         val fn = addIf(predicate, query, movie, add)
-        results = fn(movie, results)
+        results = fn(movie)(results)
     }
 
     return results
 }
 
-// addIf :: ((String, Movie) -> Boolean, String, Movie, [Movie] -> (Boolean)) -> (Movie) -> (Boolean)
-fun addIf(predicate: (String, Movie) -> Boolean, query: String, movie: Movie, add: (Movie, List<Movie>) -> (List<Movie>)): (Movie, List<Movie>) -> (List<Movie>){
-    if (predicate(query, movie)){
+fun addIf(predicate: (String, Movie) -> Boolean, query: String, movie: Movie, add: (Movie) -> (List<Movie>) -> List<Movie>): (Movie) -> (List<Movie>) -> List<Movie> {
+    if (predicate(query, movie)) {
         return add
     }
-    return fun (movie: Movie, movies: List<Movie>) = listOf<Movie>()
+    return nop()
 }
+
+fun nop() = fun(_: Movie) = fun(_: List<Movie>) = listOf<Movie>()
 
 fun matches(query: String, movie: Movie) = movie.title.contains(query)
 
