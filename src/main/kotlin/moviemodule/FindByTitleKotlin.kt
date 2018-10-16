3e5e5e46b426ecc7104cd9f6a865e6921bd03f55
jwin <jwin1968@gmail.com>
2018-10-17 00:32:59 +0200

    add type signatures... squint and see Haskell ;)

--- src/main/kotlin/moviemodule/FindByTitleKotlin.kt
+++ src/main/kotlin/moviemodule/FindByTitleKotlin.kt
@@ -1,18 +1,21 @@
 package moviemodule
 
 
-val findByTitle = { query: String ->
-    { collection: MutableList<Movie> ->
-        val predicate = matches(query)
-        filter(predicate)(collection)
-    }
-}
+val findByTitle: (String) -> (MutableList<Movie>) -> List<Movie> =
+        { query: String ->
+            { collection: MutableList<Movie> ->
+                val predicate = matches(query)
+                filter(predicate)(collection)
+            }
+        }
 
-val filter = { predicate: (Movie) -> Boolean ->
-    { collection: List<Movie> ->
-        collection.filter(predicate)
-    }
-}
+val filter: ((Movie) -> Boolean) -> (List<Movie>) -> List<Movie> =
+        { predicate: (Movie) -> Boolean ->
+            { collection: List<Movie> ->
+                collection.filter(predicate)
+            }
+        }
 
-val matches = { query: String -> { movie: Movie -> movie.title.contains(query) } }
+val matches: (String) -> (Movie) -> Boolean =
+        { query: String -> { movie: Movie -> movie.title.contains(query) } }
 
